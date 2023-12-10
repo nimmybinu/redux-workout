062da2e4373145ac2fb3d692e74d97d9e5799694
nimmy <nimmimm2014@gmail.com>
2023-12-10 21:20:40 +0530

    first commit

---
+++ src/containers/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="ui menu fixed">
+      <div className="ui container center">
+        <h2>shop</h2>
+      </div>
+    </div>
+  );
+};
+export default Header;
